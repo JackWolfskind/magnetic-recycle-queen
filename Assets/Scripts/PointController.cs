2d362460d7a90065c83b506e768ba786ddca649a
Tiborius <tibor.stachoske@gmail.com>
2018-05-19 20:38:15 +0200

    Punktestand / Gameover

---
+++ Assets/Scripts/PointController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PointController : MonoBehaviour {
+
+	public int currpoints = 0;
+
+	public Text txtPoints;
+	
+	public void TextUpdate () 
+	{
+		txtPoints.text = currpoints.ToString() + " Punkte";
+	}
+}
