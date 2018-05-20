f2785e15da575eb5bdeb1205c8316e8f7cf41ad9
Tiborius <tibor.stachoske@gmail.com>
2018-05-20 12:23:29 +0200

    Licht im Hintergrund

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -6,9 +6,24 @@ using UnityEngine.SceneManagement;
 
 public class MenuController : MonoBehaviour {
 
+	public GameObject credits;
+
 
 	public void StartGame()
 	{
 		SceneManager.LoadScene(1);
+	}
+
+	public void OpenCredits()
+	{
+		if(!credits.active)
+		{
+			credits.SetActive(true);
+		}
+		else
+		{
+			credits.SetActive(false);
+		}
+		
 	}
 }
