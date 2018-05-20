a157cff67fe4abfb8d42a62c83a5d5c6d5adbcf8
Tiborius <tibor.stachoske@gmail.com>
2018-05-20 14:57:17 +0200

    audio slider added

---
+++ Assets/Scripts/MusikManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MusikManager : MonoBehaviour {
+
+	public Slider lautstaerke;
+
+	void Start()
+	{
+		AudioListener.volume = PlayerPrefs.GetFloat("Volumen");
+	}
+	
+	void Update () 
+	{
+		AudioListener.volume = lautstaerke.value;
+		PlayerPrefs.SetFloat("Volumen", AudioListener.volume);
+	}
+}
