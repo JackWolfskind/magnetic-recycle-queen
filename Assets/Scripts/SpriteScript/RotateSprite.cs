93a78b5732968bede4b16153619d1eedf586ff51
xNoizux <39419195+xNoizux@users.noreply.github.com>
2018-05-20 13:44:32 +0200

    Scene
    die Scene umgestellt

---
+++ Assets/Scripts/SpriteScript/RotateSprite.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateSprite : MonoBehaviour
+{
+
+    void Update ()
+    {
+        transform.rotation *= Quaternion.Euler(.0f, .0f, Time.deltaTime * 100.0f);
+	}
+}
